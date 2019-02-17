4560a933cb12cde31db334f94b08f114b7a88d38
gaofenghao <gaofenghao1993@gmail.com>
2019-02-17 00:54:35 -0500

    Update Easy.sql

---
+++ Easy.sql
@@ -0,0 +1,49 @@
+-- 175	
+-- Combine Two Tables    		48.8%	Easy
+-- Test on left join, keep all from person table, even there's nothing match from address table
+
+SELECT FirstName, LastName, City, State
+FROM Person
+LEFT JOIN Address
+ON Person.PersonId = Address.PersonId;
+
+-- 176	
+-- Second Highest Salary    		25.9%	Easy	
+
+
+-- 181	
+-- Employees Earning More Than Their Managers    		44.6%	Easy	
+-- 182	
+-- Duplicate Emails    		52.0%	Easy	
+-- 183	
+-- Customers Who Never Order    		42.4%	Easy	
+-- 196	
+-- Delete Duplicate Emails    		30.3%	Easy	
+-- 197	
+-- Rising Temperature    		33.4%	Easy	
+-- 577	
+-- Employee Bonus    		56.8%	Easy	
+-- 584	
+-- Find Customer Referee    		66.3%	Easy	
+-- 586	
+-- Customer Placing the Largest Number of Orders    		64.9%	Easy	
+-- 595	
+-- Big Countries    		73.0%	Easy	
+-- 596	
+-- Classes More Than 5 Students    		34.6%	Easy	
+-- 597	
+-- Friend Requests I: Overall Acceptance Rate    		39.3%	Easy	
+-- 603	
+-- Consecutive Available Seats    		57.1%	Easy	
+-- 607	
+-- Sales Person    		54.5%	Easy	
+-- 610	
+-- Triangle Judgement    		60.8%	Easy	
+-- 613	
+-- Shortest Distance in a Line    		72.1%	Easy	
+-- 619	
+-- Biggest Single Number    		38.1%	Easy	
+-- 620	
+-- Not Boring Movies    		61.1%	Easy	
+-- 627	
+-- Swap Salary    		69.0%	Easy
