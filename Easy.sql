6747500eac171dd2e9a661f8891a39233f5bc270
gaofenghao <gaofenghao1993@gmail.com>
2019-02-21 01:48:05 -0500

    Update Easy.sql

--- Easy.sql
+++ Easy.sql
@@ -9,6 +9,7 @@ ON Person.PersonId = Address.PersonId;
 
 -- 176	
 -- Second Highest Salary    		25.9%	Easy	
+-- return specific nth record
 
 SELECT IFNULL((SELECT DISTINCT Salary
 FROM Employee
@@ -24,6 +25,7 @@ LIMIT 1, 1) AS SecondHighestSalary;
 
 -- 181	
 -- Employees Earning More Than Their Managers    		44.6%	Easy	
+-- join and comparison
 
 SELECT e1.Name AS Employee
 FROM Employee e1
@@ -32,23 +34,55 @@ WHERE e1.Salary > e2.Salary;
 
 -- 182	
 -- Duplicate Emails    		52.0%	Easy
+-- find duplicates
 
+SELECT DISTINCT Email
+FROM Person p1
+JOIN Person p2 ON p1.Email=p2.Email
+WHERE p1.Id > p2.Id;
 
+-- or
+
+SELECT Email
+FROM Person
+GROUP BY Email
+HAVING COUNT(EMail)>1;
 	
 -- 183	
 -- Customers Who Never Order    		42.4%	Easy	
 
-
+SELECT Name AS Customers
+FROM Customers
+LEFT JOIN Orders ON Customers.Id=Orders.CustomerId
+WHERE orders.Id IS NULL;
 
 -- 196	
 -- Delete Duplicate Emails    		30.3%	Easy	
+-- delete syntax
 
-
+DELETE FROM Person p1, Person p2
+WHERE p1.Email = p2.Email AND p1.Id > p2.Id;
 
 -- 197	
 -- Rising Temperature    		33.4%	Easy	
+
+-- wrong answer
+SELECT w1.Id
+FROM Weather w1
+JOIN Weather w2 ON w1.Id= (w2.Id+1)
+WHERE w1.Temperature > w2.Temperature; 
+-- correct answer
+SELECT w1.Id
+FROM Weather w1
+LEFT JOIN Weather w2
+ON DATEDIFF(w1.RecordDate, w2.RecordDate) = 1
+WHERE w1.Temperature > w2.Temperature;
+
 -- 577	
 -- Employee Bonus    		56.8%	Easy	
+
+
+
 -- 584	
 -- Find Customer Referee    		66.3%	Easy	
 -- 586	
